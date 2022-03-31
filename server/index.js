1cf86557655b56cff68c803254e2d5349e6796c7
Bogdan Protsenko <eliolooiz09@gmail.com>
2022-03-31 19:13:57 +0300

    client: added base templates; added Form component; reducers, actions etc. server: added base project file structure

--- server/index.js
+++ server/index.js
@@ -3,9 +3,25 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js'
+
 const app = express();
+
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
 
+app.use('/posts', postRoutes)
+
+const CONNECTION_URL = 'mongodb+srv://eliolooiz:elio@cluster0.htnkl.mongodb.net/Cluster0?retryWrites=true&w=majority'
+const PORT = process.env.port || 5000
+
+mongoose.connect(CONNECTION_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+.catch((error) => console.log(error.message))
+
+mongoose.set('useFindAndModify', false)
